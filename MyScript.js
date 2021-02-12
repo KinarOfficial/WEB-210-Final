7879fb91358a1bb4a8840526d7dcdbddf360caea
Hunter <Kinarunofficial@gmail.com>
2021-02-12 15:34:20 -0500

    Updated with new Source

--- MyScript.js
+++ MyScript.js
@@ -38,14 +38,14 @@ r.onclick = function() {
 
 function display(){
   if(bgs[1] == "https://res.cloudinary.com/walkerh/image/upload/v1605208081/Splash%20Images/Akali_Splash_skr9bi.jpg"){
-    document.querySelector("#infoDisplay").innerHTML = "<h1>Akali</h1><p>Akali may strike in silence, but her message will be heard loud and clear: fear the assassin with no master</p>";
+    document.querySelector("#infoDisplay").innerHTML = "<h1><a href='akali.html'>Akali</a></h1><p>Akali may strike in silence, but her message will be heard loud and clear: fear the assassin with no master</p>";
   }
   
   else if(bgs[1] == "https://res.cloudinary.com/walkerh/image/upload/v1605208079/Splash%20Images/Yone_Splash_iwfwq8.jpg"){
-    document.querySelector("#infoDisplay").innerHTML = "<h1>Yone</h1> <p>In life, he was Yone—half-brother of Yasuo, and renowned student of his village's sword school. But upon his death at the hands of his brother, he found himself hunted by a malevolent entity of the spirit realm, and was forced to slay it with its own sword.</p>";
+    document.querySelector("#infoDisplay").innerHTML = "<h1><a href='yone.html'>Yone</a></h1><p>In life, he was Yone—half-brother of Yasuo, and renowned student of his village's sword school. But upon his death at the hands of his brother, he found himself hunted by a malevolent entity of the spirit realm, and was forced to slay it with its own sword.</p>";
   }
   
   else {
-    document.querySelector("#infoDisplay").innerHTML = "<h1>Yasou</h1><p>An Ionian of deep resolve, Yasuo is an agile swordsman who wields the air itself against his enemies. As a proud young man, he was falsely accused of murdering his master—unable to prove his innocence, he was forced to slay his own brother in self-defense.</p>";
+    document.querySelector("#infoDisplay").innerHTML = "<h1><a href='yasou.html'>Yasou</a></h1><p>An Ionian of deep resolve, Yasuo is an agile swordsman who wields the air itself against his enemies. As a proud young man, he was falsely accused of murdering his master—unable to prove his innocence, he was forced to slay his own brother in self-defense.</p>";
   }
 }
